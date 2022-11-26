890735ad655d489f80140a79a205fa31c3a671b1
Eric Malcolm <eric@malcolmdigital.com>
2022-11-26 16:23:35 -0500

    adds eslint and prettier configs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,29 +3,29 @@ import React from 'react';
 import App from './App';
 
 test('renders name', () => {
-  render(<App />);
-  const name = screen.getByText(/Eric Malcolm/i);
-  expect(name).toBeInTheDocument();
+	render(<App />);
+	const name = screen.getByText(/Eric Malcolm/i);
+	expect(name).toBeInTheDocument();
 });
 
 test('renders main description', () => {
-  render(<App />);
-  const mainDescription = screen.getByText(
-    'Front-End Developer / Architect specializing in UX and Accessibility, based in Michigan.',
-  );
-  expect(mainDescription).toBeInTheDocument();
+	render(<App />);
+	const mainDescription = screen.getByText(
+		'Front-End Developer / Architect specializing in UX and Accessibility, based in Michigan.'
+	);
+	expect(mainDescription).toBeInTheDocument();
 });
 
 test('renders secondary description', () => {
-  render(<App />);
-  const secondaryDescription = screen.getByText(
-    'I help architect and develop solutions to create usable, human-friendly and accessible websites, interfaces, and apps.',
-  );
-  expect(secondaryDescription).toBeInTheDocument();
+	render(<App />);
+	const secondaryDescription = screen.getByText(
+		'I help architect and develop solutions to create usable, human-friendly and accessible websites, interfaces, and apps.'
+	);
+	expect(secondaryDescription).toBeInTheDocument();
 });
 
 test('renders LinkedIn link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/LinkedIn/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/LinkedIn/i);
+	expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className='container'>
-      <h1>Eric Malcolm</h1>
-      <h2>
-        Front-End Developer / Architect specializing in UX and Accessibility,
-        based in Michigan.
-      </h2>
-      <p>
-        I help architect and develop solutions to create usable, human-friendly
-        and accessible websites, interfaces, and apps.
-      </p>
-      <a
-        href='https://www.linkedin.com/in/ericmalcolm/'
-        aria-label="View Eric Malcolm's LinkedIn Profile"
-      >
-        LinkedIn
-      </a>
-    </div>
-  );
+function App(): JSX.Element {
+	return (
+		<div className="container">
+			<h1>Eric Malcolm</h1>
+			<h2>
+				Front-End Developer / Architect specializing in UX and Accessibility,
+				based in Michigan.
+			</h2>
+			<p>
+				I help architect and develop solutions to create usable, human-friendly
+				and accessible websites, interfaces, and apps.
+			</p>
+			<a
+				href="https://www.linkedin.com/in/ericmalcolm/"
+				aria-label="View Eric Malcolm's LinkedIn Profile"
+			>
+				LinkedIn
+			</a>
+		</div>
+	);
 }
 
 export default App;
