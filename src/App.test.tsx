f9d88db48864cea7e760e9a89a1489ec4e7094a2
Eric Malcolm <eric@malcolmdigital.com>
2022-11-26 18:46:40 -0500

    adds jest axe automated accessibility tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,16 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe'
+
 import App from './App';
 
-test('renders name', () => {
+test('App renders name', () => {
 	render(<App />);
 	const name = screen.getByText(/Eric Malcolm/i);
 	expect(name).toBeInTheDocument();
 });
 
-test('renders main description', () => {
+test('App renders main description', () => {
 	render(<App />);
 	const mainDescription = screen.getByText(
 		'Front-End Developer / Architect specializing in UX and Accessibility, based in Michigan.'
@@ -16,7 +18,7 @@ test('renders main description', () => {
 	expect(mainDescription).toBeInTheDocument();
 });
 
-test('renders secondary description', () => {
+test('App renders secondary description', () => {
 	render(<App />);
 	const secondaryDescription = screen.getByText(
 		'I help architect and develop solutions to create usable, human-friendly and accessible websites, interfaces, and apps.'
@@ -24,8 +26,16 @@ test('renders secondary description', () => {
 	expect(secondaryDescription).toBeInTheDocument();
 });
 
-test('renders LinkedIn link', () => {
+test('App renders LinkedIn link', () => {
 	render(<App />);
 	const linkElement = screen.getByText(/LinkedIn/i);
 	expect(linkElement).toBeInTheDocument();
 });
+
+test('App renders with no accessibility violations', async () => {
+	expect.extend(toHaveNoViolations)
+  const { container } = render(<App/>)
+  const results = await axe(container)
+  
+  expect(results).toHaveNoViolations()
+})
