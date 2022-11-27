6f268c33b34b115f5be16465cae258180fcd76ff
Eric Malcolm <eric@malcolmdigital.com>
2022-11-27 03:12:47 -0500

    reverts back the index of the project and test coverage

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 import BasicBio from './components/BasicBio/BasicBio';
 import basicBioData from './content/basicBioData.json';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
