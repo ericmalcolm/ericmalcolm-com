499007385b8291aca27f1dd6aa8d3fa35228bdb8
Eric Malcolm <eric@malcolmdigital.com>
2022-11-28 08:04:18 -0500

    adds more content to billboard component

--- src/components/Billboard/Billboard.tsx
+++ src/components/Billboard/Billboard.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import './Billboard.scss';
 
+import EricMalcolmBackground from './../../assets/eric-malcolm_billboard.jpg';
+import EricMalcolm480 from './../../assets/eric-malcolm_billboard_480x610.jpg';
+import EricMalcolm670 from './../../assets/eric-malcolm_billboard_670x470.jpg';
+import MLogo from './../../assets/eric-malcolm_m-logo.png';
+
+// TODO: Tests.
+
 export const Billboard = (): JSX.Element => {
 	return (
-		<section className="billboard">
+		<section
+			className="billboard"
+			style={{
+				// TODO: manage this with state and hooks to handleResize window.innerWidth > 767
+				// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+				backgroundImage: `url(${EricMalcolmBackground as string})`,
+			}}
+		>
 			<section className="billboard-header">
 				<div className="billboard-header__content">
-					<img
-						src="https://via.placeholder.com/50"
-						alt=""
-						role="presentation"
-					/>
+					<img src={MLogo} alt="" role="presentation" />
 					<h1>Eric Malcolm</h1>
-					<section>links</section>
 				</div>
 				<div className="billboard-header__image billboard-header__image--mobile">
-					<img
-						src="https://via.placeholder.com/370x470"
-						alt="Eric Malcolm Profile"
-					/>
+					<img src={EricMalcolm480} alt="Eric Malcolm Profile" />
 				</div>
 				<div className="billboard-header__image billboard-header__image--tablet">
-					<img
-						src="https://via.placeholder.com/670x470"
-						alt="Eric Malcolm Profile"
-					/>
+					<img src={EricMalcolm670} alt="Eric Malcolm Profile" />
 				</div>
 			</section>
 			<section className="billboard-description">
@@ -36,12 +39,26 @@ export const Billboard = (): JSX.Element => {
 					I help architect and develop solutions to create usable,
 					human-friendly and accessible websites, interfaces, and apps.
 				</p>
-				<a
-					href="https://www.linkedin.com/in/ericmalcolm/"
-					aria-label="View Eric Malcolm's LinkedIn Profile"
-				>
-					LinkedIn
-				</a>
+				<p>
+					<a
+						href="https://www.linkedin.com/in/ericmalcolm/"
+						aria-label="View Eric Malcolm's LinkedIn Profile"
+					>
+						LinkedIn
+					</a>{' '}
+					<a
+						href="/Eric-Malcolm_Resume.pdf"
+						aria-label="View Eric Malcolm's Resume in PDF Form"
+					>
+						Resume (PDF)
+					</a>
+					<a
+						href="https://github.com/ericmalcolm"
+						aria-label="View Eric Malcolm's GitHub Repos"
+					>
+						GitHub
+					</a>
+				</p>
 			</section>
 		</section>
 	);
